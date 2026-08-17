86e0407460c771926ce2a45333fdccf4df137397
Fabriziolm <Fabriziolm@users.noreply.github.com>
2026-08-17 11:22:45 -0500

    use official Express by DCS app icon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="dcs-shell-v3";
+const CACHE="dcs-shell-v4";
 const APP_SCOPE=new URL(self.registration.scope).pathname;
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll([APP_SCOPE,`${APP_SCOPE}manifest.webmanifest`,`${APP_SCOPE}dcs-app-icon.png`])).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
