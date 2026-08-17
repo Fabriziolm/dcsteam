cece5200e61f0b0f28c47414fe600b0346296663
Fabriziolm <Fabriziolm@users.noreply.github.com>
2026-08-17 10:57:24 -0500

    fix standalone app icon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="dcs-shell-v1";
+const CACHE="dcs-shell-v2";
 const APP_SCOPE=new URL(self.registration.scope).pathname;
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll([APP_SCOPE,`${APP_SCOPE}manifest.webmanifest`,`${APP_SCOPE}dcs-app-icon.svg`])).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
